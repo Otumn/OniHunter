a8af3831d35036181a141c7a61ab23729eb74442
Nicolo <nicoceriani@gmail.com>
2019-01-27 15:09:36 +0100

    Trails WIP

---
+++ Assets/SCRIPTS/Feedback/FX/DashTrail/DashTrailFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pagann.OniHunter
+{
+    public class DashTrailFX : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] slashObjects;
+        [SerializeField] private Player player;
+
+        private void Update()
+        {
+            for (int i = 0; i < slashObjects.Length; i++)
+            {
+                slashObjects[i].SetActive(player.IsDashing);
+            }
+        }
+    }
+}
---
+++ Assets/SCRIPTS/Feedback/FX/DashTrail/DashTrailsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pagann.OniHunter
+{
+    public class DashTrailsManager : MonoBehaviour
+    {
+        [SerializeField] private int currentlyEquippedTrail = 0;
+        [Space(10)]
+        [SerializeField] private DashTrailFX[] trails;
+
+        private void Update()
+        {
+            UpdateTrailEquipped();
+        }
+        
+        private void UpdateTrailEquipped()
+        {
+            for (int i = 0; i < trails.Length; i++)
+            {
+                if (i == currentlyEquippedTrail)
+                {
+                    trails[i].gameObject.SetActive(true);
+                }
+
+                else
+                {
+                    trails[i].gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
